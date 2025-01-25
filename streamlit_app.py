cf0c651abf1b965dfe7db594c689fe9f48e03499
tktesting-for-business <taka3chijp@gmail.com>
2025-01-25 14:36:11 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,4 @@ import streamlit as st
 x = 10
 'x: ', x 
 
-    usecols = st.multiselect(
-        '何番目の列を解析の対象にしますか？',
-        [0, 3, 4, 5, 6],
-        [0, 3, 4, 5, 6])
+usecols = st.multiselect('何番目の列を解析の対象にしますか？',[0, 3, 4, 5, 6],[0, 3, 4, 5, 6])
