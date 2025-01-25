4cf23dfda70faa8695814cd0114d222c45edcc4d
tktesting-for-business <taka3chijp@gmail.com>
2025-01-25 14:54:33 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,13 @@
 import streamlit as st
 import streamlit.components.v1 as stc
 
+# 画像保存先のディレクトリを設定
+IMG_PATH = '/content/images'
+
+# 画像保存先のディレクトリが存在しない場合は作成
+if not os.path.exists(IMG_PATH):
+    os.makedirs(IMG_PATH)
+
 st.title("Embedding Dify app in Streamlit")
 # Case 1の場合
 st.write("## Case 1.")
