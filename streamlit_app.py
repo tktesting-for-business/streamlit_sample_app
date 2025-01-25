e0f37c7fc511d85d40332340b8db96e196a6fc93
tktesting-for-business <taka3chijp@gmail.com>
2025-01-25 14:39:44 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,22 @@ import streamlit as st
 x = 10
 'x: ', x 
 
-usecols = st.multiselect('何番目の列を解析の対象にしますか？',[0, 3, 4, 5, 6],[0, 3, 4, 5, 6])
+<!DOCTYPE html>
+<html>
+<head>
+<title>Red Rectangle</title>
+<style>
+  canvas {
+    border: 3px solid pink;
+  }
+</style>
+</head>
+<body>
+<script>
+import streamlit as st
+x = 10
+'x: ', x 
+</script>
+
+</body>
+</html>
