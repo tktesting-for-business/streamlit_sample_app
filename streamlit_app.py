b2348bf889f45f0db75fbd895c23bd12be8e1226
tktesting-for-business <taka3chijp@gmail.com>
2025-01-25 16:24:53 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,8 +8,8 @@ from PIL import Image
 # Streamlit app タイトル
 st.title("Drawable Canvas Demo")
 
-img = Image.open('aaa.png')
-st.image(img, caption='サンプル',use_column_width=True)
+#img = Image.open('aaa.png')
+#st.image(img, caption='サンプル',use_column_width=True)
 
 # キャンバスの設定
 canvas_result = st_canvas(
