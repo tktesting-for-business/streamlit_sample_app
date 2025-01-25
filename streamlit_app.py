a251a2ff6d323aac27f478fab1069fd3145b2a0e
tktesting-for-business <taka3chijp@gmail.com>
2025-01-25 20:56:20 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -77,8 +77,9 @@ def get_dify_response(query: str) -> str:
     response = requests.post(BASE_URL, headers=headers, json=data)
     response.raise_for_status()
     
-    return response.json()['output']
-    # return response.text
+    # return response.json()['output']
+    # return response.json()['output']
+    return response.text
 
 if __name__ == "__main__":
     query = "aaaaa"
