38cacb1b3c100c967d0ef675d01196de19f85a41
tktesting-for-business <taka3chijp@gmail.com>
2025-01-25 14:47:54 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,21 @@
-
-
-<html>
-<head>
-<title>Red Rectangle</title>
-
-</head>
-<body>
-<script>
- import streamlit as st
  x = 10
  'x: ', x 
-</script>
 
-</body>
-</html>
+import streamlit as st
+import streamlit.components.v1 as stc
+
+st.title("Embedding Dify app in Streamlit")
+# Case 1の場合
+st.write("## Case 1.")
+# iFrameのHTMLコード
+html_code = """
+<iframe
+ src="https://udify.app/chatbot/jGTwlTKwnlbTFs0M"
+ style="width: 100%; height: 100%; min-height: 700px"
+ frameborder="0"
+ allow="microphone">
+</iframe>
+"""
+# HTMLをStreamlitアプリに埋め込む
+stc.html(html_code, height=800)
+
