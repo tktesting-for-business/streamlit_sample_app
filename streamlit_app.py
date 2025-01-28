25d60d12a416d0a7bb33936522fc1fbe89475ce7
tktesting-for-business <taka3chijp@gmail.com>
2025-01-29 08:44:19 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,11 +45,13 @@ data = {
     "inputs": {"query": 'こんにちは。今日はいい天気ですね'},
     "response_mode": "streaming",  
     "user": "taka3chijp@gmail.com"
-    "files": { 
+    "files": [
+        { 
             "type": "image", 
             "transfer_method": "remote_url", 
             "url": "aaa.jpg" 
         } 
+]     
 }
 
 response = requests.post(url, headers=headers, json=data)
